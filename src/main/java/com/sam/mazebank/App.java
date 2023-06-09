c121b65cee2a3df9dffe4ac61b8a7ff728146863
devMunyi <samunyi90@gmail.com>
2023-06-09 17:27:49 +0300

    Add Design For login.fxml

--- src/main/java/com/sam/mazebank/App.java
+++ src/main/java/com/sam/mazebank/App.java
@@ -1,9 +1,17 @@
 package com.sam.mazebank;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private Parent root;
+    private Scene scene;
 
     public static void main(String[] args){
         launch(args);
@@ -11,6 +19,43 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("App is Up & Running");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/login.fxml"));
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+            // System.out.println("App is Up & Running");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            try {
+                exitApp(stage);
+            }catch (Exception e2){
+                System.out.println(e2);
+            }
+        });
+    }
+
+
+    public void exitApp(Stage stage){
+        // Close Alert
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close App");
+        // alert.setHeaderText("About to Exit App");
+        alert.setHeaderText("Are You Sure You Want to Exit");
+        // alert.setContentText("Are Sure You Want to Exit the Application?");
+        if(alert.showAndWait().get() == ButtonType.OK){
+            try {
+                Platform.exit();
+                stage.close();
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
     }
 }
--- src/main/java/com/sam/mazebank/controllers/LoginController.java
+++ src/main/java/com/sam/mazebank/controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.sam.mazebank.controllers;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_address_lbl, error_lbl;
+    public TextField password_fld;
+    public Button login_btn;
 }
