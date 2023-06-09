a7c02cff02da63f51559c8f8c3d40a32cc7d9f91
devMunyi <samunyi90@gmail.com>
2023-06-09 09:39:40 +0300

    Add Main Method to App Class

--- src/main/java/com/sam/mazebank/App.java
+++ src/main/java/com/sam/mazebank/App.java
@@ -1,5 +1,16 @@
 package com.sam.mazebank;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class App extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("App is Up & Running");
+    }
 }
