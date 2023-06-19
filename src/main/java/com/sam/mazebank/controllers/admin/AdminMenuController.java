1704211746f453122743eaa11ee71169c766c057
devMunyi <samunyi90@gmail.com>
2023-06-19 10:23:18 +0300

    Initial Commit

---
+++ src/main/java/com/sam/mazebank/controllers/admin/AdminMenuController.java
@@ -0,0 +1,13 @@
+package com.sam.mazebank.controllers.admin;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
