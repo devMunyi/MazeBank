a66393db90ed5bef4f7445f4a690ec9b7b5479a7
devMunyi <samunyi90@gmail.com>
2023-06-20 11:38:06 +0300

    Initial Commit

---
+++ src/main/java/com/sam/mazebank/controllers/client/TransactionCellController.java
@@ -0,0 +1,13 @@
+package com.sam.mazebank.controllers.client;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
