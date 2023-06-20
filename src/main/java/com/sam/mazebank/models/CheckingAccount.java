b5c0654e6d24c1eb7433b4ad1f83f5e3f52841e8
devMunyi <samunyi90@gmail.com>
2023-06-20 17:00:14 +0300

    Initial Commit

---
+++ src/main/java/com/sam/mazebank/models/CheckingAccount.java
@@ -0,0 +1,17 @@
+package com.sam.mazebank.models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class CheckingAccount extends Account{
+
+    private final IntegerProperty transactionLimit;
+    public CheckingAccount(String owner, String accountNumber, double balance, int transLimit) {
+        super(owner, accountNumber, balance);
+        this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", transLimit);
+    }
+
+    public IntegerProperty transactionLimitProperty() {
+        return transactionLimit;
+    }
+}
---
+++ src/main/java/com/sam/mazebank/models/SavingsAccount.java
@@ -0,0 +1,17 @@
+package com.sam.mazebank.models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account{
+
+    private final DoubleProperty withdrawalLimit;
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit", withdrawalLimit);
+    }
+
+    public DoubleProperty withdrawalLimitProperty() {
+        return withdrawalLimit;
+    }
+}
